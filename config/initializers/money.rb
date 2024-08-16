d586a02022f00ded183112fc8ec191f43cd703ef
Vanam <raghavendra.gv.vanam@gmail.com>
2024-08-16 14:42:32 +0530

    Below are the changes: 1. Removed the blank lines

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -2,16 +2,12 @@
 require 'money/bank/open_exchange_rates_bank'
 
 MoneyRails.configure do |config|
-
   # To set the default currency
-  #
   config.default_currency = :usd
   oxr = Money::Bank::OpenExchangeRatesBank.new(Money::RatesStore::Memory.new)
   oxr.app_id = 'your app id from https://openexchangerates.org/signup'
   oxr.update_rates
   config.default_bank = oxr
-
-
   # Add exchange rates to current money bank object.
   # (The conversion rate refers to one direction only)
   #
