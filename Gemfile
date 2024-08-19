0f25851e622f891874d39fb46c0c6542e9c10dc9
Vanam <raghavendra.gv.vanam@gmail.com>
2024-08-19 11:12:35 +0530

    Below are the changes: 1. Commented out oxr.update_rates as there is issue while running the server 2. Devise gem is provided in the gem file and Gemfile.lock is updated with the latest changes 3. Devise gem views are added primarily for sign in, sign up & also changing passwords 4. development.rb is updated with the settings for devise gem 5. Devise is created with user model & corresponding migrations and schema is added 6. Routes file is updated with root pointing to home controller 7. Home controller and index view is created

--- Gemfile
+++ Gemfile
@@ -84,3 +84,5 @@ group :development do
   # gem "spring"
 end
 
+
+gem "devise", "~> 4.9"
--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -6,7 +6,9 @@ MoneyRails.configure do |config|
   config.default_currency = :usd
   oxr = Money::Bank::OpenExchangeRatesBank.new(Money::RatesStore::Memory.new)
   oxr.app_id = 'your app id from https://openexchangerates.org/signup'
-  oxr.update_rates
+  # Uncomment below line when app_id is there for generating
+  # exchange rates
+  # oxr.update_rates
   config.default_bank = oxr
   # Add exchange rates to current money bank object.
   # (The conversion rate refers to one direction only)
