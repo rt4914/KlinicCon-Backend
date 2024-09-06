de9a427f1c965ddc5f878817dc756dec534a8240
Vanam <raghavendra.gv.vanam@gmail.com>
2024-09-06 22:55:49 +0530

    Below are the changes: 1. Degree model with name column 2. Schema and migration file related to degree model

---
+++ db/migrate/20240906172152_create_degrees.rb
@@ -0,0 +1,8 @@
+class CreateDegrees < ActiveRecord::Migration[7.1]
+  def change
+    create_table :degrees do |t|
+      t.string :name
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_06_044059) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_06_172152) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_06_044059) do
     t.string "state"
     t.string "country"
     t.string "pin_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "degrees", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
