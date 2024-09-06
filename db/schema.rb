c75a98c77ebe66b68f5e7d2aa05312c2e4408cae
Vanam <raghavendra.gv.vanam@gmail.com>
2024-09-07 00:04:51 +0530

    Below are the changes: 1. registration_council.rb model with name as column 2. schema.rb updated with newer table details 3. Migration file for registration council

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_06_172152) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_06_183131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_06_172152) do
   end
 
   create_table "degrees", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "registration_councils", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
